122775bc2b37370ea1b9def83275a6b1017f7ae9
110870/Galasso <110870@tecnicatura.frc.utn.edu.ar>
2022-08-02 01:39:06 -0300

    App finalizada 2.0

--- README.md
+++
@@ -1,6 +0,0 @@
-# pokedex
-A Pokedex made with React Native, using PokeAPI 
-
-This project was made in order to complete the full course of Platzi Web Platform.
-
-The default user is 'mataias' and the password is '123456'. This can be changed in the src/utils/userDB.js file.
--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,20 +1,45 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState, useCallback } from "react";
+import { View, Text } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GetPokemonsFavoriteApi } from "../api/favorite";
+import useAuth from "../hooks/useAuth";
+import { getPokemonInfo } from "../api/pokemon";
+import PokemonList from "../components/PokemonList";
+import NotLogged from "../components/Auth/NotLogged";
 
 export default function Favorite() {
-  const [favorites, setFavorites] = useState(null);
+  const [favorites, setFavorites] = useState([]);
+  const { auth } = useAuth();
 
-  const getFavorites = async () => {
-    const response = await GetPokemonsFavoriteApi();
-    console.log(response);
-  };
+  useFocusEffect(
+    useCallback(() => {
+      if (auth) {
+        (async () => {
+          const response = await GetPokemonsFavoriteApi();
+          const pokemonsArray = [];
+          for await (const id of response) {
+            const pokemonDetails = await getPokemonInfo(id);
+            pokemonsArray.push({
+              id: pokemonDetails.id,
+              name: pokemonDetails.name,
+              type: pokemonDetails.types[0].type.name,
+              order: pokemonDetails.order,
+              image:
+                pokemonDetails.sprites.other["official-artwork"].front_default,
+            });
+          }
+          setFavorites(pokemonsArray);
+        })();
+      }
+    }, [auth])
+  );
 
-  return (
+  return !auth ? (
+    <NotLogged />
+  ) : (
     <SafeAreaView>
-      <Text>Favorite</Text>
-      <Button title="get favorites" onPress={getFavorites}></Button>
+      <PokemonList pokemons={favorites} />
     </SafeAreaView>
   );
 }
