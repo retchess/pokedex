1be653f2d799605f76180cb66685bab8d970c88b
110870/Galasso <110870@tecnicatura.frc.utn.edu.ar>
2022-08-02 01:27:14 -0300

    App finalizada

--- src/components/Auth/UserData.js
+++ src/components/Auth/UserData.js
@@ -1,9 +1,26 @@
 import { View, Text, StyleSheet, Button } from "react-native";
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import useAuth from "../../hooks/useAuth";
+import { GetPokemonsFavoriteApi } from "../../api/favorite";
 
 export default function UserData() {
   const { auth, logout } = useAuth();
+  const [total, setTotal] = useState(0);
+
+  useFocusEffect(
+    useCallback(() => {
+      (async () => {
+        try {
+          const response = await GetPokemonsFavoriteApi();
+          setTotal(response.length);
+        } catch (error) {
+          setTotal(0);
+        }
+      })();
+    }, [])
+  );
+
   return (
     <View style={styles.content}>
       <View style={styles.titleBlock}>
@@ -15,7 +32,7 @@ export default function UserData() {
         <ItemMenu title="Nombre" text={`${auth.firstName} ${auth.lastName}`} />
         <ItemMenu title="Username" text={auth.username} />
         <ItemMenu title="Email" text={auth.email} />
-        <ItemMenu title="Total de favoritos" text={`0 Pokemons`} />
+        <ItemMenu title="Total de favoritos" text={`${total} Pokemons`} />
       </View>
       <Button title="Cerrar sesion" onPress={logout} color="#ff3e3e" />
     </View>
